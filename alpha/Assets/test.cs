6d1d4bc78bbc0dcc664dfc997f49b3a7c946ed7f
Hexadecimal <xiuransun@hotmail.com>
2016-06-30 16:16:54 +0800

    # 构建测试场景 - 导入插件iTween - 导入一张测试的图片
    ### 杂项
    - 删除原定主场景，日后构建
    
    Signed-off-by: Hexadecimal <xiuransun@hotmail.com>

---
+++ alpha/Assets/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class test : MonoBehaviour {
+
+	public float delay = 200f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = transform.position;
+		transform.position = new Vector3(x.x, x.y + (float)Math.Sin(Time.time+delay));
+	}
+}
